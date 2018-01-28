d015ccf4e95eb269c53e3598e8b285aa2fe3e306
Daniel Smith <danielsmith4483@gmail.com>
2018-01-28 00:08:34 -0600

    Add material-ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import RaisedButton from "material-ui/RaisedButton";
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <RaisedButton label="Material UI" />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
