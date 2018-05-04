9c0dce931aeeb63421e3ff718a324388fbfdc689
Daniel Smith <danielsmith4483@gmail.com>
2018-05-03 23:08:58 -0500

    Implement basic 1RM calculator

--- src/Containers/RepMaxContainer/index.js
+++ src/Containers/RepMaxContainer/index.js
@@ -1,33 +1,63 @@
 import React, { Component } from "react";
 
-import { connect } from "react-redux";
-import { setChosenWeight, setChosenReps } from "Containers/RepMaxContainer/actions";
+import TextField from "material-ui/TextField";
 
-import WeightPickerComponent from "Components/WeightPickerComponent";
-import RepPickerComponent from "Components/RepPickerComponent";
+import { connect } from "react-redux";
 
 import { repMax } from 'rep-max';
 
 const mapStateToProps = state => {
   return {
-    chosenWeight: state.chosenWeight,
-    chosenReps: state.chosenReps
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    setChosenWeight: chosenWeight => dispatch(setChosenWeight(chosenWeight)),
-    setChosenReps: chosenReps => dispatch(setChosenReps(chosenReps))
   };
 };
 
 class RepMaxContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { chosenWeight: 0, chosenReps: 0 };
+
+    this.onChangeWeight = this.onChangeWeight.bind(this);
+    this.onChangeReps = this.onChangeReps.bind(this);
+  }
+
+  onChangeWeight(event, newValue) {
+    this.setState({ chosenWeight: newValue });
+  }
+
+  onChangeReps(event, newValue) {
+    this.setState({ chosenReps: newValue });
+  }
+
+  getRepMax() {
+    try {
+      return parseFloat(Number(repMax(this.state.chosenWeight, this.state.chosenReps)).toFixed(2));
+    } catch (e) {
+      return "N/A"
+    }
+  }
+
   render() {
     return (
       <div>
-        <WeightPickerComponent onChange={this.props.setChosenWeight} />
-        <RepPickerComponent onChange={this.props.setChosenReps} />
+      <TextField
+        hintText="225"
+        floatingLabelText="Enter weight"
+        onChange={(e, v) => this.onChangeWeight(e, v)}
+      />
+      
+      <TextField
+        hintText="5"
+        floatingLabelText="Enter reps"
+        onChange={(e, v) => this.onChangeReps(e, v)}
+      />
+
+      <p>One-Rep Max: <strong>{this.getRepMax()}</strong></p>
       </div>
     );
   }
